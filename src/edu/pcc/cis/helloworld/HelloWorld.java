47ec7b6b2ce4698ce5d612202996ab0387132253
Denise Martinez <denise.martinez3@pcc.edu>
2022-02-03 23:25:45 -0800

    added main method that prints hellp world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,4 +4,7 @@ package edu.pcc.cis.helloworld;
  * Created by Denise Martinez on 2/3/2022
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 }
