ab50d0cd1cb7ee84a4e9c09d7356da37d3b15612
Denise Martinez <denise.martinez3@pcc.edu>
2022-02-03 23:30:34 -0800

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,10 +1,13 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Created by Denise Martinez on 2/3/2022
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
